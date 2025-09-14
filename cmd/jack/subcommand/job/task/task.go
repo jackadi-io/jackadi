62fe3e6284186800db26b600801ce4460ca71edb
Kevin Petremann <k@kpmail.fr>
2025-09-14 18:33:27 +0200

    fix: make CI happy

--- cmd/jack/subcommand/job/task/task.go
+++ cmd/jack/subcommand/job/task/task.go
@@ -58,13 +58,13 @@ func RunCommand() *cobra.Command {
 			if len(args) < 2 {
 				err := fmt.Errorf("requires at least %d arg(s), only received %d", 2, len(args))
 				fmt.Println(style.RenderError(err.Error()))
-				cmd.Help()
+				_ = cmd.Help()
 				os.Exit(1)
 			}
 			if args[0] == "" {
 				err := errors.New("target must not be empty")
 				fmt.Println(style.RenderError(err.Error()))
-				cmd.Help()
+				_ = cmd.Help()
 				os.Exit(1)
 			}
 			return nil
